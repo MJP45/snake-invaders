491dfb6b4bcd26ba135a64a8b6cdd7b3279f9344
Max Jaeckel-Parker <max.jaeckel-parker@nology.io>
2019-10-10 12:35:44 +0100

    added new files for testing, WIP - new branch to break classes into seperate files

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-
-const button = document.getElementById("button").addEventListener("click", attackSnakes);
-onclick = target.beenHit;
-
-
 // go add hp to snakes and represent it globaly 
 // link random action to a specific snake and mark that snake as hit showing a reduction in hp. 
 
-
 // Classes
-
 // Snake 
-// take damage
-// not touchable when dead 
 class Snake {
   hp;
   damage;
-
   beenHit() {
     this.hp = (this.hp - this.damage);
   }
-}
-
-// Button
-// randomly selects a snake each time pressed
-// inflicts dammage concurrent with individual snake damage parameters 
+};
 
 // Queen
 // when hp = 0 all wasps dead
-
 class Queen extends Snake {  
   constructor(hp, damage){
     super();
@@ -35,13 +20,15 @@ class Queen extends Snake {
     this.damage = damage;
   }
 
-  queenDead(){
-    if (this.hp <= 0){
-        Snake.hp = 0;
-    } else {
-      return this.hp;
+  // trigger this when queen hp = 0
+  // outside the function 
+    killEveryone(){
+      if (this.hp <= 0){
+          snakes.forEach(snake => {
+            snake.hp = 0;
+          });
+      } 
     }
-  }
 }
 // set a for loop for beenHit/check-queenDead/beenHit etc...
     // const playgame = () => {
@@ -53,16 +40,7 @@ class Queen extends Snake {
 
     // playgame();
 
-const basilisk = new Queen(80, 7);
-const queen = [basilisk];
-
-// basilisk.queenDead();
-// basilisk.beenHit();
-console.log(basilisk.hp);
-
 // Worker
-// hp = 68
-// damage = -10
 class Worker extends Snake { 
   constructor(hp, damage){
     super();
@@ -71,44 +49,67 @@ class Worker extends Snake {
   }
 }
 
-const worker1 = new Worker(68, 10);
-const worker2 = new Worker(68, 10);
-const worker3 = new Worker(68, 10);
-const worker4 = new Worker(68, 10);
-const worker5 = new Worker(68, 10);
-const workers = [worker1, worker2, worker3, worker4, worker5];
-// worker1.beenHit();
-console.log(worker1.hp);
-
 // Drone
-// hp = 60
-// damage = -12
 class Drone extends Snake {
   constructor(hp, damage){
     super();
     this.hp = hp;
     this.damage = damage;
   }
-}
-const drone1 = new Drone(60, 12);
-const drone2 = new Drone(60, 12);
-const drone3 = new Drone(60, 12);
-const drone4 = new Drone(60, 12);
-const drone5 = new Drone(60, 12);
-const drone6 = new Drone(60, 12);
-const drone7 = new Drone(60, 12);
-const drone8 = new Drone(60, 12);
-const drones = [drone1, drone2, drone3, drone4, drone5, drone6, drone7, drone8];
-// drone1.beenHit()
-console.log(drone1.hp);
+};
 
-// Make an array of 1 queen, 5 workers and 8 drones, and log it to the console.
+const basilisk = new Queen(80, 7);
+const queen = [basilisk];
+// basilisk.queenDead();
+
+const numWorkers = 5;
+let workers = [];
+for(let i = 0; i < numWorkers; i++){
+    workers.push(new Worker(68,10)); 
+};
+console.log(workers[0].hp);
+
+const numDrones = 8;
+let drones = [];
+for(let i = 0; i < numDrones; i++){ 
+    drones.push(new Drone(60,12));
+};
+
 const snakes = queen.concat(workers, drones);
 console.log(snakes);
+// selecting a random element from the array of snakes to be attacked.
+attackSnakes = () => {
+  const randomIndex = Math.floor(Math.random() * snakes.length);
+  const target = snakes[randomIndex];
+  target.beenHit();
+  // hitDomElement(randomIndex);
+  displayScores(snakes.hp);
+};
 
-// selecting a random element from the array of snakes.
-const target = snakes[Math.floor(Math.random() * snakes.length)];
-target.beenHit();
+// access snake elements in the dom 
+// hitDomElement = (randomIndex) => {
+//   let elements = [...document.getElementsByTagName("div")];
+//   elements[randomIndex].innerHTML = "HIT";
+// };
 
-console.log(target);
+//give snake elements hp im the dom
+//make function to do this
+
+
+displayScores = () => {
+  console.log("i'm displaying a score")
+let htmlArray = [...document.querySelectorAll('.points')];
+    for (i in snakes) {
+      htmlArray[i].innerHTML = snakes[i].hp;
+    };
+  };
+
+
+
+
+
+document.getElementById("button").addEventListener("click", attackSnakes);
+
+
+
 
